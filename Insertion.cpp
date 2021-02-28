fc7db0d6298aedd30936dd78acadb355b11e1117
Manish Kumar Santoshi <manishkumarsantoshi@gmail.com>
2021-02-28 22:09:36 +0530

    Update Insertion.cpp

--- Insertion.cpp
+++ Insertion.cpp
@@ -14,7 +14,7 @@ int main(){
         key=ar[i];
         for(j=i-1;(key<ar[j])&&(j>=0);j--){
             ar[j+1]=ar[j];
-            }
+        }
         ar[j+1]=key;
     }
     for(i=0;i<n;i++)
