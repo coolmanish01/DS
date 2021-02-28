951cb0422d81f731a7ee4d4ead69b097bd2c4214
Manish Kumar Santoshi <manishkumarsantoshi@gmail.com>
2021-02-28 18:40:44 +0530

    Add files via upload

---
+++ Insertion.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int ar[20],t,n;
+    cout<<"Enter Size of Array : ";
+    cin>>n;
+    int i,j;
+    for(i=0;i<n;i++){
+        cout<<"Enter the "<<i+1<<" Element of Array : ";
+        cin>>ar[i];
+    }
+    int key;
+    for(i=1;i<n;i++){
+        key=ar[i];
+        for(j=i-1;(key<ar[j])&&(j>=0);j--){
+            ar[j+1]=ar[j];
+            }
+        ar[j+1]=key;
+    }
+    for(i=0;i<n;i++)
+        cout<<ar[i]<<" ";
+    return 0;
+}
