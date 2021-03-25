b48477f59095ad41d0272ece2613ddf34bf9c4ff
Manish Kumar Santoshi <manishkumarsantoshi@gmail.com>
2021-03-25 06:05:55 +0530

    Update DeleteMiddle.c

--- DeleteMiddle.c
+++ DeleteMiddle.c
@@ -35,12 +35,10 @@ int main(){
     struct Node *head;
     struct Node *second;
     struct Node *third;
-    struct Node *fourth;
 
     head = (struct Node *)malloc(sizeof(struct Node));
     second = (struct Node *)malloc(sizeof(struct Node));
     third = (struct Node *)malloc(sizeof(struct Node));
-    fourth = (struct Node *)malloc(sizeof(struct Node));
 
     head->data = 10;
     head->next = second;
@@ -49,15 +47,12 @@ int main(){
     second->next = third;
 
     third->data = 30;
-    third->next = fourth;
-
-    fourth->data = 40;
-    fourth->next = NULL;
+    third->next = NULL;
 
     printf("Linked list before deletion\n");
     traversal(head);
 
-    head = deleteMiddle(head,2);
+    head = deleteMiddle(head,1);
     printf("Linked list after deletion\n");
     traversal(head);
     return 0;
