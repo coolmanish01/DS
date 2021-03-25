78ce12510f652eefcfc8d4b19623fbf59169cf46
Manish Kumar Santoshi <manishkumarsantoshi@gmail.com>
2021-03-25 06:15:07 +0530

    Add files via upload

---
+++ DELETEWHOLE.C
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct Node{
+    int data;
+    struct Node *next;
+};
+
+void traversal(struct Node *ptr){
+    while (ptr != NULL){
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+struct Node *deleteList(struct Node *head)
+{
+    struct Node *temp;
+    while(head != NULL)
+    {
+        temp = head;
+        head = head->next;
+
+        free(temp);
+    }
+
+    printf("SUCCESSFULLY DELETED ALL NODES OF LINKED LIST\n");
+    return head;
+}
+int main(){
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = fourth;
+
+    fourth->data = 40;
+    fourth->next = NULL;
+
+    printf("Linked list before deletion\n");
+    traversal(head);
+
+    head = deleteList(head);
+
+    return 0;
+}
